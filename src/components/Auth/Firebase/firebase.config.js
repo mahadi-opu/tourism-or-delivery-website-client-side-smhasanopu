f7333bc558cd53b124057680a338ec5602f60c9e
Syed Mahadi Hasan Opu <smhasanopu@gmail.com>
2021-10-31 23:52:15 +0600

    3rd commit

--- src/components/Auth/Firebase/firebase.config.js
+++ src/components/Auth/Firebase/firebase.config.js
@@ -4,14 +4,14 @@
 
 
 // console.log(process.env);
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
 
+const firebaseConfig = {
+    apiKey: "AIzaSyC5v8p2cUMdLlCwBsO1RAEOTTLtqM8oCG0",
+    authDomain: "travel-tour-af3a7.firebaseapp.com",
+    projectId: "travel-tour-af3a7",
+    storageBucket: "travel-tour-af3a7.appspot.com",
+    messagingSenderId: "438273552149",
+    appId: "1:438273552149:web:d8cdbee9bda631d129831c"
+  };
 
 export default firebaseConfig;
